da62ad02e6907a04baedc5cb4dddf3d7408dfc79
Nsquik <46610139+Nsquik@users.noreply.github.com>
2020-04-08 05:02:56 +0200

    ...
    ...

--- src/components/Login/LoginContainer.js
+++ src/components/Login/LoginContainer.js
@@ -6,7 +6,7 @@ import context from "../../context/TokenContext";
 let hash = window.location.hash
   .substring(1)
   .split("&")
-  .reduce(function(initial, item) {
+  .reduce(function (initial, item) {
     if (item) {
       var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
@@ -24,7 +24,6 @@ const error = urlParams.get("error");
 
 const LoginContainer = (props) => {
   const TokenContext = useContext(context);
-  console.log(hash.access_token);
 
   if (hash.access_token) {
     Cookies.set("token", hash.access_token, { expires: 1 / 48 });
